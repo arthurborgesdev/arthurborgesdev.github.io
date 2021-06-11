bbf9550f3140a08e059424ceac5fb3731d3a6dc3
Arthur Borges <arthurborges.dev@gmail.com>
2021-06-11 19:29:10 -0300

    Add formData fields to test the send of email

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ $('#emailForm').on('submit', function(event) {
   event.preventDefault(); // prevent reload
   
   var formData = new FormData(this);
-  formData.append('service_id', 'YOUR_SERVICE_ID');
-  formData.append('template_id', 'YOUR_TEMPLATE_ID');
-  formData.append('user_id', 'YOUR_USER_ID');
+  formData.append('service_id', 'service_osvhifd');
+  formData.append('template_id', 'template_819k8xq');
+  formData.append('user_id', 'user_Hqjg6CGWG7bozRdUpnZDd');
+  formData.append('to_name', 'Arthur Borges');
+  formData.append('from_name', $("#emailForm :input[name='name']"));
+  formData.append('from_email', $("#emailForm :input[name='email']"));
+  formData.append('message', $("#emailForm :input[name='message']"));
 
   $.ajax('https://api.emailjs.com/api/v1.0/email/send-form', {
       type: 'POST',
